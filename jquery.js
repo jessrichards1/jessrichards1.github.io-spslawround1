7ba2fe1c512421a566dafff49375537054a5f4bc
jessrichards1 <jessr@bu.edu>
2019-12-08 20:54:41 -0500

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+
+$("li").hover(function() {
+            $(".navWords").css("background-color", "green");
+        }, function() {
+            $(".navWords").css("background-color", "yellow");
+        });
+    });
+
+
+  ;
+
+
+
+
+
+
+
+});
